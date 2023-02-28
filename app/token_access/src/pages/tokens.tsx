7250dc8966df83d18ccb8a63947ebef95b3314a6
Riki <ricardocr987@gmail.com>
2023-02-28 01:15:30 +0100

    getTokensOnSale from a user pubkey

--- app/token_access/src/pages/tokens.tsx
+++ app/token_access/src/pages/tokens.tsx
@@ -1,8 +1,9 @@
-import { ACCOUNTS_DATA_LAYOUT, AccountType, AssetArgs } from "@/utils";
+import { ACCOUNTS_DATA_LAYOUT, AccountType, AssetArgs, TOKEN_ACCESS_PROGRAM_ID_PK } from "@/utils";
 import { getAssetPubkey } from "@/utils/helpers";
 import { AccountLayout, RawAccount, TOKEN_PROGRAM_ID } from "@solana/spl-token";
 import { useConnection, useWallet } from "@solana/wallet-adapter-react";
 import { Connection, PublicKey } from "@solana/web3.js";
+import bs58 from "bs58";
 import { useEffect, useState } from "react";
 
 async function getTokens(publicKey: PublicKey, connection: Connection) {
@@ -34,19 +35,45 @@ async function getTokens(publicKey: PublicKey, connection: Connection) {
     return tokensData
 }
 
+async function getTokensOnSale(publicKey: PublicKey, connection: Connection) {
+    const tokensOnSale: AssetArgs[] = []
+    const encodedTokensOnSale = await connection.getProgramAccounts(
+        TOKEN_ACCESS_PROGRAM_ID_PK,
+        {
+            filters: [
+                {
+                    memcmp: {
+                        bytes: publicKey.toString(),
+                        offset: 212, // authority offset, to get assets this user is selling
+                    },
+                },
+            ],
+        },
+    )
+
+    for (const tokenAccount of encodedTokensOnSale){
+        const assetData = ACCOUNTS_DATA_LAYOUT[AccountType.Asset].deserialize(tokenAccount.account)[0]
+        tokensOnSale.push(assetData)
+    }
+
+    return tokensOnSale
+}
+
 const UserTokensPage = () => {
     const wallet = useWallet()
     const { connection } = useConnection()
     const [tokens, setTokens] = useState([]);
+    const [tokensOnSale, setTokensOnSale] = useState([]);
 
     useEffect(() => {
         const setAccountState = async () => {
           if (wallet.connected) {
             const tokensData = await getTokens(wallet.publicKey, connection)
             setTokens(tokensData)
+            const tokensOnSale = await getTokensOnSale(wallet.publicKey, connection)
+            setTokensOnSale(tokensOnSale)
           }
         }
-        setAccountState()
     }, [wallet.connected]);
     
     return (
